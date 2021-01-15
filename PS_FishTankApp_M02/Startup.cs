ec5986bb848cabe73366914a75fc097487b471b1
Sam Niyazi <sam2003@niyazi.com>
2021-01-15 08:26:56 -0600

    Xompleted M02.

--- PS_FishTankApp_M02/Startup.cs
+++ PS_FishTankApp_M02/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 using PS_FishTankApp_M02.Services;
 
 namespace PS_FishTankApp_M02
@@ -24,17 +25,44 @@ namespace PS_FishTankApp_M02
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        // 01/15/2021 08:06 am - SSN - [20210115-0803] - [003] - M02-09 - Into the pipeline 
+        // Inject loggerFactory
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
+            // Not needed.  
+            //       loggerFactory.AddConsole(minLevel: LogLevel.Information);
+
+            var logger = loggerFactory.CreateLogger("ssn_info_20210115a");// Category
+
+
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
 
+            // 01/15/2021 08:04 am - SSN - [20210115-0803] - [002] - M02-09 - Into the pipeline 
+            //app.Run(async (context) =>
+            app.Use(async (context, next) =>
+            {
+                logger.LogInformation("20210115-0811-a: Before next in first middleware;");
+
+                await context.Response.WriteAsync("Hello World!");
+                await next();
+
+                logger.LogInformation("20210115-0811-b: After next in first middleware;");
+
+            });
+
+            // 01/15/2021 08:03 am - SSN - [20210115-0803] - [001] - M02-09 - Into the pipeline 
+
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                logger.LogInformation("20210115-0811-c: Before next in second middleware;");
+                await context.Response.WriteAsync("Hello World again!");
+                logger.LogInformation("20210115-0811-d: After next in second middleware;");
             });
+
         }
     }
 }
